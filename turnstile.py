e07c0fd6afe4141f3949d79c8be4fbcb8d2d8fe3
matthewma1522-debug <matthewma1522@gmail.com>
2026-01-06 20:57:47 -0800

    1

--- turnstile.py
+++ turnstile.py
@@ -1,1 +1,34 @@
 # write code to implement a turnstile
+LOCKED = "locked"
+UNLOCKED = "unlocked"
+
+state=LOCKED
+
+
+
+
+while True:
+    order = input("Enter command (insertcoin, push, exit): ")
+    if state == LOCKED:
+        if order == "insertcoin":
+            print("Coin inserted. You can now push the turnstile.")
+            state == UNLOCKED
+        elif order == "push":
+            print("locked insert a coin first")
+        else:
+            print("invalid")
+
+
+    elif state == UNLOCKED:
+        if order == "insertcoin": 
+            print("already unlocked, dont need coin")
+        elif order == "push":
+            print("you went through because you pushed. ")
+        
+    
+    elif order == "exit":
+        print("Exiting turnstile.")
+        break
+    else:
+        print("invalid order,try again.")
+        
